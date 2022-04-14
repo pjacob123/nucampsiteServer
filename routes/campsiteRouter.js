43abe5c88fed0982634e49d957930221e538001c
Paul Jacob <paul.jacob@icloud.com>
2022-04-14 17:25:31 -0400

    Week 3 Assignment

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -16,7 +16,7 @@ campsiteRouter.route('/')
             .catch(err => next(err));
     })
 
-    .post(authenticate.verifyUser, (req, res, next) => {
+    .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
         Campsite.create(req.body)
             .then(campsite => {
                 console.log('Campsite Created', campsite);
@@ -32,7 +32,7 @@ campsiteRouter.route('/')
         res.end('PUT operation not support on /campsites');
     })
 
-    .delete(authenticate.verifyUser, (req, res, next) => {
+    .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
         Campsite.deleteMany()
             .then(response => {
                 res.statusCode = 200;
@@ -59,7 +59,7 @@ campsiteRouter.route('/:campsiteId')
         res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
     })
 
-    .put(authenticate.verifyUser, (req, res, next) => {
+    .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
         Campsite.findByIdAndUpdate(req.params.campsiteId, {
             $set: req.body
         }, { new: true })
@@ -71,7 +71,7 @@ campsiteRouter.route('/:campsiteId')
             .catch(err => next(err));
     })
 
-    .delete(authenticate.verifyUser, (req, res, next) => {
+    .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
         Campsite.findByIdAndDelete(req.params.campsiteId)
             .then(response => {
                 res.statusCode = 200;
@@ -127,7 +127,7 @@ campsiteRouter.route('/:campsiteId/comments')
     })
 
 
-    .delete(authenticate.verifyUser, (req, res, next) => {
+    .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
         Campsite.findById(req.params.campsiteId)
             .then(campsite => {
                 if (campsite) {
@@ -179,28 +179,35 @@ campsiteRouter.route('/:campsiteId/comments/:commentId')
     .put(authenticate.verifyUser, (req, res, next) => {
         Campsite.findById(req.params.campsiteId)
             .then(campsite => {
-                if (campsite && campsite.comments.id(req.params.commentId)) {
-                    if (req.body.rating) {
-                        campsite.comments.id(req.params.commentId).rating = req.body.rating;
+                if ((campsite.comments.id(req.params.commentId).author._id).equals(req.user._id)) {
+                    if (campsite && campsite.comments.id(req.params.commentId)) {
+                        if (req.body.rating) {
+                            campsite.comments.id(req.params.commentId).rating = req.body.rating;
+                        }
+                        if (req.body.text) {
+                            campsite.comments.id(req.params.commentId).text = req.body.text;
+                        }
+                        campsite.save()
+                            .then(campsite => {
+                                res.statusCode = 200;
+                                res.setHeader('Content-Type', 'application/json');
+                                res.json(campsite);
+                            })
+                            .catch(err => next(err));
+                    } else if (!campsite) {
+                        err = new Error(`Campsite ${req.params.campsiteId} not found`);
+                        err.status = 404;
+                        return next(err);
+                    } else {
+                        err = new Error(`Comment ${req.params.commentId} not found`);
+                        err.status = 404;
+                        return next(err);
                     }
-                    if (req.body.text) {
-                        campsite.comments.id(req.params.commentId).text = req.body.text;
-                    }
-                    campsite.save()
-                        .then(campsite => {
-                            res.statusCode = 200;
-                            res.setHeader('Content-Type', 'application/json');
-                            res.json(campsite);
-                        })
-                        .catch(err => next(err));
-                } else if (!campsite) {
-                    err = new Error(`Campsite ${req.params.campsiteId} not found`);
-                    err.status = 404;
-                    return next(err);
                 } else {
-                    err = new Error(`Comment ${req.params.commentId} not found`);
-                    err.status = 404;
+                    err = new Error('You are not authorized to update this comment');
+                    err.status = 401;
                     return next(err);
+
                 }
             })
             .catch(err => next(err));
@@ -208,24 +215,32 @@ campsiteRouter.route('/:campsiteId/comments/:commentId')
     .delete(authenticate.verifyUser, (req, res, next) => {
         Campsite.findById(req.params.campsiteId)
             .then(campsite => {
-                if (campsite && campsite.comments.id(req.params.commentId)) {
-                    campsite.comments.id(req.params.commentId).remove();
-                    campsite.save()
-                        .then(campsite => {
-                            res.statusCode = 200;
-                            res.setHeader('Content-Type', 'application/json');
-                            res.json(campsite);
-                        })
-                        .catch(err => next(err));
-                } else if (!campsite) {
-                    err = new Error(`Campsite ${req.params.campsiteId} not found`);
-                    err.status = 404;
-                    return next(err);
+                if ((campsite.comments.id(req.params.commentId).author._id).equals(req.user._id)) {
+                    if (campsite && campsite.comments.id(req.params.commentId)) {
+                        campsite.comments.id(req.params.commentId).remove();
+                        campsite.save()
+                            .then(campsite => {
+                                res.statusCode = 200;
+                                res.setHeader('Content-Type', 'application/json');
+                                res.json(campsite);
+                            })
+                            .catch(err => next(err));
+                    } else if (!campsite) {
+                        err = new Error(`Campsite ${req.params.campsiteId} not found`);
+                        err.status = 404;
+                        return next(err);
+                    } else {
+                        err = new Error(`Comment ${req.params.commentId} not found`);
+                        err.status = 404;
+                        return next(err);
+                    }
                 } else {
-                    err = new Error(`Comment ${req.params.commentId} not found`);
-                    err.status = 404;
+                    err = new Error('You are not authorized to delete this comment');
+                    err.status = 401;
                     return next(err);
+
                 }
+
             })
             .catch(err => next(err));
     });
--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -16,7 +16,7 @@ partnerRouter.route('/')
             .catch(err => next(err));
     })
 
-    .post(authenticate.verifyUser, (req, res, next) => {
+    .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
         Partner.create(req.body)
             .then(partner => {
                 console.log('Partner Created', partner);
@@ -32,7 +32,7 @@ partnerRouter.route('/')
         res.end('PUT operation not support on /partners');
     })
 
-    .delete(authenticate.verifyUser, (req, res, next) => {
+    .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
         Partner.deleteMany()
             .then(response => {
                 res.statusCode = 200;
@@ -58,7 +58,7 @@ partnerRouter.route('/:partnerId')
         res.end(`POST operation not supported on /partners/${req.params.partnerId}`);
     })
 
-    .put(authenticate.verifyUser, (req, res, next) => {
+    .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
         Partner.findByIdAndUpdate(req.params.partnerId, {
             $set: req.body
         }, { new: true })
@@ -70,7 +70,7 @@ partnerRouter.route('/:partnerId')
             .catch(err => next(err));
     })
 
-    .delete(authenticate.verifyUser, (req, res, next) => {
+    .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
         Partner.findByIdAndDelete(req.params.partnerId)
             .then(response => {
                 res.statusCode = 200;
