27428c3aa5d6c02c1a489fcb74f2a6c32e310a2f
Paul Jacob <paul.jacob@icloud.com>
2022-04-20 13:11:35 -0400

    Passport Facebook

--- models/user.js
+++ models/user.js
@@ -14,7 +14,8 @@ const userSchema = new Schema({
     admin: {
         type: Boolean,
         default: false
-    }
+    },
+    facebookID: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
